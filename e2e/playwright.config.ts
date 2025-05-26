35d73101d8ca832227af2e0208f9c13d8098ccc1
John Martin <john.itvn@gmail.com>
2025-05-26 21:23:25 +0000

    chore: update timeout for serve backend and frontend

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -32,13 +32,13 @@ export default defineConfig({
             command: "yarn nx run frontend:serve",
             url: "http://localhost:4200",
             cwd: workspaceRoot,
-            timeout: 120000,
+            timeout: 300000,
           },
           {
             command: "yarn nx run backend:serve",
             url: "http://localhost:3000/health",
             cwd: workspaceRoot,
-            timeout: 120000,
+            timeout: 300000,
           },
         ],
   projects: [

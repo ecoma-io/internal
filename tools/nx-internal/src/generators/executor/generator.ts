ecoma-io/application

chore: initial repository setup [skip ci]

tools/nx-internal/src/generators/executor/generator.ts
@@ -1,0 +1,72 @@
+import {
+  formatFiles,
+  generateFiles,
+  logger,
+  Tree,
+  updateJson,
+} from "@nx/devkit";
+import { existsSync } from "fs";
+import * as path from "path";
+
+import { executorGeneratorSchema } from "./schema";
+
+export async function executorGenerator(
+  tree: Tree,
+  options: executorGeneratorSchema
+) {
+  const generatorRoot = getGeneratorRoot(options.name);
+
+  if (existsSync(generatorRoot)) {
+    logger.error(`Executor ${options.name} already exists`);
+  } else {
+    generateExecutorFiles(tree, generatorRoot, {
+      ...options,
+      className: getClassName(options.name),
+    });
+    updateExecutorsJson(tree, options);
+    await formatFiles(tree);
+  }
+}
+
+function getGeneratorRoot(name: string): string {
+  return path.join(__dirname, "../../executors", name);
+}
+
+function getClassName(name: string): string {
+  return name
+    .split("-")
+    .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+    .join("");
+}
+
+function generateExecutorFiles(
+  tree: Tree,
+  generatorRoot: string,
+  options: executorGeneratorSchema & { className: string }
+): void {
+  generateFiles(
+    tree,
+    path.join(__dirname, "files"),
+    generatorRoot.replace(tree.root, ""),
+    options
+  );
+}
+
+function updateExecutorsJson(
+  tree: Tree,
+  options: executorGeneratorSchema
+): void {
+  const executorsJsonPath = path
+    .join(__dirname, "../../../executors.json")
+    .replace(tree.root, "");
+  updateJson(tree, executorsJsonPath, (json) => {
+    json["executors"][options.name] = {
+      implementation: `./src/executors/${options.name}/executor`,
+      schema: `./src/executors/${options.name}/schema.json`,
+      description: `${options.name} executor`,
+    };
+    return json;
+  });
+}
+
+export default executorGenerator;

tools/nx-internal/src/generators/executor/schema.d.ts
@@ -1,0 +1,3 @@
+export interface executorGeneratorSchema {
+  name: string;
+}

tools/nx-internal/src/generators/generator/generator.spec.ts
@@ -1,0 +1,100 @@
+import { Tree, logger } from "@nx/devkit";
+import { existsSync } from "fs";
+import * as path from "path";
+
+import { generatorGenerator } from "./generator";
+import { GeneratorGeneratorSchema } from "./schema";
+
+interface Generator {
+  factory: string;
+  schema: string;
+  description: string;
+}
+
+interface GeneratorJson {
+  generators: {
+    [key: string]: Generator;
+  };
+}
+
+jest.mock("fs", () => ({
+  existsSync: jest.fn(),
+}));
+
+jest.mock("@nx/devkit", () => ({
+  formatFiles: jest.fn(),
+  generateFiles: jest.fn(),
+  logger: {
+    error: jest.fn(),
+  },
+  updateJson: jest.fn(),
+}));
+
+describe("generatorGenerator", () => {
+  let tree: Tree;
+  const options: GeneratorGeneratorSchema = { name: "testgenerator" };
+
+  beforeEach(() => {
+    tree = {
+      root: "/root",
+    } as Tree;
+    jest.clearAllMocks();
+  });
+
+  it("should log an error if the generator name is invalid", async () => {
+    await generatorGenerator(tree, { name: "invalid-name" });
+
+    expect(logger.error).toHaveBeenCalledWith(
+      'Generator name "invalid-name" is invalid. It must contain only lowercase letters.'
+    );
+  });
+
+  it("should log an error if the generator already exists", async () => {
+    (existsSync as jest.Mock).mockReturnValue(true);
+
+    await generatorGenerator(tree, options);
+
+    expect(logger.error).toHaveBeenCalledWith(
+      "Generator testgenerator already exists"
+    );
+  });
+
+  it("should generate files and update JSON if the generator name is valid and does not exist", async () => {
+    (existsSync as jest.Mock).mockReturnValue(false);
+
+    await generatorGenerator(tree, options);
+
+    expect(logger.error).not.toHaveBeenCalled();
+    expect(require("@nx/devkit").generateFiles).toHaveBeenCalledWith(
+      tree,
+      path.join(__dirname, "files"),
+      path.join(__dirname, "..", options.name).replace(tree.root, ""),
+      options
+    );
+    expect(require("@nx/devkit").updateJson).toHaveBeenCalled();
+    expect(require("@nx/devkit").formatFiles).toHaveBeenCalledWith(tree);
+  });
+
+  it("should add generator to generator.json", async () => {
+    (existsSync as jest.Mock).mockReturnValue(false);
+
+    await generatorGenerator(tree, options);
+
+    expect(require("@nx/devkit").updateJson).toHaveBeenCalledWith(
+      tree,
+      path.join(__dirname, "../../../generators.json").replace(tree.root, ""),
+      expect.any(Function)
+    );
+
+    const updateJsonCallback = (require("@nx/devkit").updateJson as jest.Mock)
+      .mock.calls[0][2];
+    const json: GeneratorJson = { generators: {} };
+    updateJsonCallback(json);
+
+    expect(json.generators[options.name]).toEqual({
+      factory: `./src/generators/${options.name}/generator`,
+      schema: `./src/generators/${options.name}/schema.json`,
+      description: `${options.name} generator`,
+    });
+  });
+});

tools/nx-internal/src/generators/generator/schema.d.ts
@@ -1,0 +1,3 @@
+export interface GeneratorGeneratorSchema {
+  name: string;
+}

ecoma-io/internal

tools/nx-internal/src/generators/executor/generator.ts
@@ -1,0 +1,72 @@
+import {
+  formatFiles,
+  generateFiles,
+  logger,
+  Tree,
+  updateJson,
+} from "@nx/devkit";
+import { existsSync } from "fs";
+import * as path from "path";
+
+import { IExecutorGeneratorSchema } from "./schema";
+
+export async function executorGenerator(
+  tree: Tree,
+  options: IExecutorGeneratorSchema
+) {
+  const generatorRoot = getGeneratorRoot(options.name);
+
+  if (existsSync(generatorRoot)) {
+    logger.error(`Executor ${options.name} already exists`);
+  } else {
+    generateExecutorFiles(tree, generatorRoot, {
+      ...options,
+      className: getClassName(options.name),
+    });
+    updateExecutorsJson(tree, options);
+    await formatFiles(tree);
+  }
+}
+
+function getGeneratorRoot(name: string): string {
+  return path.join(__dirname, "../../executors", name);
+}
+
+function getClassName(name: string): string {
+  return name
+    .split("-")
+    .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+    .join("");
+}
+
+function generateExecutorFiles(
+  tree: Tree,
+  generatorRoot: string,
+  options: IExecutorGeneratorSchema & { className: string }
+): void {
+  generateFiles(
+    tree,
+    path.join(__dirname, "files"),
+    generatorRoot.replace(tree.root, ""),
+    options
+  );
+}
+
+function updateExecutorsJson(
+  tree: Tree,
+  options: IExecutorGeneratorSchema
+): void {
+  const executorsJsonPath = path
+    .join(__dirname, "../../../executors.json")
+    .replace(tree.root, "");
+  updateJson(tree, executorsJsonPath, (json) => {
+    json["executors"][options.name] = {
+      implementation: `./src/executors/${options.name}/executor`,
+      schema: `./src/executors/${options.name}/schema.json`,
+      description: `${options.name} executor`,
+    };
+    return json;
+  });
+}
+
+export default executorGenerator;

tools/nx-internal/src/generators/executor/schema.d.ts
@@ -1,0 +1,3 @@
+export interface IExecutorGeneratorSchema {
+  name: string;
+}

tools/nx-internal/src/generators/generator/generator.spec.ts
@@ -1,0 +1,100 @@
+import { Tree, logger } from "@nx/devkit";
+import { existsSync } from "fs";
+import * as path from "path";
+
+import { generatorGenerator } from "./generator";
+import { IGeneratorGeneratorSchema } from "./schema";
+
+interface IGenerator {
+  factory: string;
+  schema: string;
+  description: string;
+}
+
+interface IGeneratorJson {
+  generators: {
+    [key: string]: IGenerator;
+  };
+}
+
+jest.mock("fs", () => ({
+  existsSync: jest.fn(),
+}));
+
+jest.mock("@nx/devkit", () => ({
+  formatFiles: jest.fn(),
+  generateFiles: jest.fn(),
+  logger: {
+    error: jest.fn(),
+  },
+  updateJson: jest.fn(),
+}));
+
+describe("generatorGenerator", () => {
+  let tree: Tree;
+  const options: IGeneratorGeneratorSchema = { name: "testgenerator" };
+
+  beforeEach(() => {
+    tree = {
+      root: "/root",
+    } as Tree;
+    jest.clearAllMocks();
+  });
+
+  it("should log an error if the generator name is invalid", async () => {
+    await generatorGenerator(tree, { name: "invalid-name" });
+
+    expect(logger.error).toHaveBeenCalledWith(
+      'Generator name "invalid-name" is invalid. It must contain only lowercase letters.'
+    );
+  });
+
+  it("should log an error if the generator already exists", async () => {
+    (existsSync as jest.Mock).mockReturnValue(true);
+
+    await generatorGenerator(tree, options);
+
+    expect(logger.error).toHaveBeenCalledWith(
+      "Generator testgenerator already exists"
+    );
+  });
+
+  it("should generate files and update JSON if the generator name is valid and does not exist", async () => {
+    (existsSync as jest.Mock).mockReturnValue(false);
+
+    await generatorGenerator(tree, options);
+
+    expect(logger.error).not.toHaveBeenCalled();
+    expect(require("@nx/devkit").generateFiles).toHaveBeenCalledWith(
+      tree,
+      path.join(__dirname, "files"),
+      path.join(__dirname, "..", options.name).replace(tree.root, ""),
+      options
+    );
+    expect(require("@nx/devkit").updateJson).toHaveBeenCalled();
+    expect(require("@nx/devkit").formatFiles).toHaveBeenCalledWith(tree);
+  });
+
+  it("should add generator to generator.json", async () => {
+    (existsSync as jest.Mock).mockReturnValue(false);
+
+    await generatorGenerator(tree, options);
+
+    expect(require("@nx/devkit").updateJson).toHaveBeenCalledWith(
+      tree,
+      path.join(__dirname, "../../../generators.json").replace(tree.root, ""),
+      expect.any(Function)
+    );
+
+    const updateJsonCallback = (require("@nx/devkit").updateJson as jest.Mock)
+      .mock.calls[0][2];
+    const json: IGeneratorJson = { generators: {} };
+    updateJsonCallback(json);
+
+    expect(json.generators[options.name]).toEqual({
+      factory: `./src/generators/${options.name}/generator`,
+      schema: `./src/generators/${options.name}/schema.json`,
+      description: `${options.name} generator`,
+    });
+  });
+});

tools/nx-internal/src/generators/generator/schema.d.ts
@@ -1,0 +1,3 @@
+export interface IGeneratorGeneratorSchema {
+  name: string;
+}